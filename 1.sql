ea864997520093395eda8282d7eafb12c8e203e5
Shiva Surya Vasan <110565005+ShivaSuryaVasan@users.noreply.github.com>
2024-03-06 13:49:10 +0530

    Delete 1.sql

--- 1.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE record_example(given_id IN TEXT) AS $$
-DECLARE
-rec record;
-BEGIN
-SELECT * INTO rec FROM products WHERE id = given_id;
-RAISE NOTICE 'price: %', rec.price;
-END
-$$ LANGUAGE plpgsql;
